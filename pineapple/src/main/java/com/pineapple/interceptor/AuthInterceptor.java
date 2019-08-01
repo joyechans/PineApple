8a886722f99732d1aa312c5e1e496660c5d70d21
goott6-3 <goott6-3@goott6-3-PC>
2019-08-01 16:44:03 +0900

    19.08.01 프로젝트 생성	-예찬

---
+++ pineapple/src/main/java/com/pineapple/interceptor/AuthInterceptor.java
@@ -0,0 +1,37 @@
+package com.pineapple.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AuthInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+		
+		String uri = req.getRequestURI();
+		
+		if (uri.contains("/pr-upload")) {
+			HttpSession session = req.getSession();
+			if (session.getAttribute("loginuser") == null) {		
+				resp.sendRedirect("/pineapple/account/login");
+				return false; 
+			}
+		}
+		
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {		
+	}
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {		
+	} 
+
+}
